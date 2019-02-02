c1c2c0344e4596743a4cfd8490979e5222652ba7
shivangg <xshivangx@gmail.com>
2019-02-02 20:23:53 +0530

    feat: Add error handling middleware

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', routes);
 
+// middleware to handle error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(422).send({
+    error: `${err.name}: ${err.message}`
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`I'm listening to ${PORT}`);
--- routes/api.js
+++ routes/api.js
@@ -3,29 +3,30 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // get a list of ninjas from db
-router.get('/ninjas', (req, res) => {
+router.get('/ninjas', (req, res, next) => {
   res.send({
     type: 'GET'
   });
 });
 
 // add a new ninja to db
-router.post('/ninjas', (req, res) => {
+router.post('/ninjas', (req, res, next) => {
   Ninja.create(req.body)
     .then( ninja => {
       res.send(ninja);
     })
+    .catch(next);
 });
 
 // update a ninja from the db
-router.put('/ninjas/:id', (req, res) => {
+router.put('/ninjas/:id', (req, res, next) => {
   res.send({
     type: 'PUT'
   });
 });
 
 // delete a ninja from db
-router.delete('/ninjas', (req, res) => {
+router.delete('/ninjas', (req, res, next) => {
   res.send({
     type: 'DELETE'
   });
